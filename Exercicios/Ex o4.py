824569b05baceea6a7af60e6f86591940f5cb880
BeatrizHonorato <obbeatriz4@gmail.com>
2023-04-01 12:18:52 -0300

    Ex_04 Dissecando uma variavel

---
+++ Exercicios/Ex o4.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informaçoes possiveis sobre ele.
+
+print("Só tem espacos? ",a.isspace())
+print("É um número?",a.isnumeric())
+print("É alfabético?",a.isalpha())
+print("É alfanumérico?",a.isalnum())
